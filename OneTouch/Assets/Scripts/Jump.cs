fa1b4ef01af475216bcceb0a5f426eeed2170d07
cloudgaga <cloudgaga750@gmail.com>
2017-12-01 16:33:40 +0900

    12011
    12011

--- OneTouch/Assets/Scripts/Jump.cs
+++ OneTouch/Assets/Scripts/Jump.cs
@@ -8,10 +8,11 @@ public class Jump : MonoBehaviour {
 	//bool isJump =false;
 
 	Button Notes;
-	Animator anim;
+    Animator Playeranim;
+    Animation startAnim;
 
-	//创建一个数组,在界面那里把预制物体拖进NotePrefab里
-	public GameObject[] NotePrefabs;
+    //创建一个数组,在界面那里把预制物体拖进NotePrefab里
+    public GameObject[] NotePrefabs;
 
     public GameObject StartEffect;
     public GameObject OverEffect;
@@ -19,15 +20,14 @@ public class Jump : MonoBehaviour {
     //计时器
     float timer = 0;
 
-
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator>();
-        Destroy(StartEffect,1.5f);
+        Playeranim = GetComponent<Animator>();
+        Destroy(StartEffect,4f);
     }
     private IEnumerator ShowA()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(5);
         Update();
     }
 
@@ -45,21 +45,40 @@ public class Jump : MonoBehaviour {
             GameObject NotePrefab = NotePrefabs[Random.Range(0, 4)];
 
             //随机预制体的位置
-            Vector3 point = Camera.main.ViewportToWorldPoint (new Vector3 (Random.value, Random.value,-Camera.main.transform.position.z));
+            Vector2 point = Camera.main.ViewportToWorldPoint (new Vector2 (Random.value, Random.value));
 
 			//生成预制体
             GameObject notes = Instantiate(NotePrefab, point, NotePrefab.transform.rotation) as GameObject;
+
             notes.transform.SetParent(FindObjectOfType<Canvas>().transform);
 
-            notes.GetComponent<Button> ().onClick.AddListener (delegate() {
-				Destroy (notes);
-
-				//StartCoroutine(Anim());
-				anim.CrossFade("Jump",1.0f);
-			});
-
-			//五秒销毁预制体
-			Destroy (notes, 5);
+            //五秒销毁预制体
+            Destroy (notes, 5);
 		}
-	} 
+
+        //点击note响应事件
+        //notes.GetComponent<Button>().onClick.AddListener(delegate ()
+        //{
+        //    Destroy(notes);
+        //    Playeranim.SetBool("IsJump", true);
+        //});
+
+        if (Playeranim == null) return;
+
+        AnimatorStateInfo stateInfo = Playeranim.GetCurrentAnimatorStateInfo(0);
+
+        //按下鼠标左键时响应该方法  
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("~~~");
+            Playeranim.SetInteger("States", 1);
+        }
+
+        if (stateInfo.normalizedTime >= 1.0f)
+        {
+            //播放完毕，要执行的内容
+            //Playeranim.SetBool("IsJump", false);
+            Playeranim.SetInteger("States", 0);
+        }
+    } 
 }
