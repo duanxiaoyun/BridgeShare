d9e093d7d4e32621dba9e550ce88910f85e6e50a
duanxiaoyun <cloudgaga750@gmail.com>
2017-12-10 13:35:21 +0900

    20171210
    1210

--- OneTouch/Assets/Scripts/JumpControll.cs
+++ OneTouch/Assets/Scripts/JumpControll.cs
@@ -4,66 +4,92 @@ using UnityEngine.UI;
 using UnityEditor;
 using System;
 
-public class JumpControll : MonoBehaviour {
+public class JumpControll : MonoBehaviour
+{
 
     bool isJump = false;
+    private bool isMiss;
 
-    Button Notes;
-
-    Animator Playeranim;
+    private Animator Playeranim;
     Animation startAnim;
 
     //数组,NotePrefabs
-    public GameObject[] NotePrefabs;
-    public GameObject[] NotePrefabsEffs;
+    [SerializeField]
+    private GameObject[] NotePrefabs;
+    [SerializeField]
+    private GameObject[] NotePrefabsEffs;
 
     int noteTimer = 5;
-    public Image notelevel;
-    public Sprite[] NoteLevels;
+    [SerializeField]
+    private Image notelevel;
+    [SerializeField]
+    private Sprite[] NoteLevels;
 
     GameObject notes;
     GameObject notesEff;
 
-    public GameObject StartEffect;
-    public GameObject OverEffect;
+    [SerializeField]
+    private GameObject StartEffect;
+    [SerializeField]
+    private GameObject OverEffect;
+
+    [SerializeField]
+    private Sprite[] numbers;
+    [SerializeField]
+
+    int[] nums = new int[2];
+
+    [SerializeField]
+    private GameObject TimeNum;
+    [SerializeField]
+    private float timer = 30.0f;
 
     //计时器
-    float timer = 0;
+    float Notetimer = 0;
 
     //HP
-    public int PlayerHp = 1000;
-    public Text PlayerHpUI;
+    [SerializeField]
+    private int PlayerHp = 1000;
+    [SerializeField]
+    private Text PlayerHpUI;
 
-    // Use this for initialization
-    void Start ()
+    private void Awake()
+    {
+        GameManagers.Instance.JumpControll= this;
+    }
+
+        // Use this for initialization
+        void Start ()
     {
         Playeranim = GetComponent<Animator>();
-        Destroy(StartEffect,4f);
+        Destroy(StartEffect,4f);//游戏开始特效
     }
     private IEnumerator ShowA()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(5);//游戏5秒后再开始
         Update();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        Notetimer += Time.deltaTime;
 
         PlayerHpUI.text = PlayerHp.ToString();
 
-        if (timer >= 2)
+        if (Notetimer >= 2)
         {
-            timer = 0;
+            Notetimer = 0;
 
-                //找到预制体
+            //找到预制体
             int noteindex = UnityEngine.Random.Range(0, 4);
 
             GameObject NotePrefab = NotePrefabs[noteindex];
 
-                //随机预制体的位置和范围
-            //Vector2 point = Camera.main.ViewportToWorldPoint (new Vector2 (Random.value, Random.value));
+            //随机预制体的位置和范围
+            //Vector2 point = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
 
             float x = UnityEngine.Random.Range(-1100, 1100);
             float y = UnityEngine.Random.Range(-300, 500);
@@ -72,46 +98,74 @@ public class JumpControll : MonoBehaviour {
 
             Vector2 point = new Vector2(x, y);
 
-                //生成预制体
+            //生成预制体
             notes = Instantiate(NotePrefab, point, NotePrefab.transform.rotation) as GameObject;
             notes.transform.SetParent(FindObjectOfType<Canvas>().transform);
 
-                //点击note响应事件
+            //点击note响应事件
             notes.GetComponent<Button>().onClick.AddListener(delegate ()
             {
                 Destroy(notes);
+                isJump = true;
+                Playeranim.SetTrigger("Jump");
 
                 //notes 爆炸效果
                 notesEff = Instantiate(NotePrefabsEffs[noteindex], point, NotePrefab.transform.rotation) as GameObject;
                 Destroy(notesEff, 2);
-                notelevel.GetComponent<Image>().sprite = Instantiate(NoteLevels[4], point, NotePrefab.transform.rotation) as Sprite;
-                notelevel.transform.position = point;
+                //notelevel.GetComponent<Image>().sprite = Instantiate(NoteLevels[4], point, NotePrefab.transform.rotation) as Sprite;
+                //notelevel.transform.position = point;
             });
 
-                //五秒销毁预制体
+            //五秒销毁预制体
             Destroy(notes, noteTimer);
-            timer -= Time.deltaTime;
-            noteTimer = Convert.ToInt32(timer.ToString("0.0").Split('.')[0]);   
-            if (noteTimer == 0)
-            {
-                notelevel.GetComponent<Image>().sprite = Instantiate(NoteLevels[4], point, NotePrefab.transform.rotation) as Sprite;
-                notelevel.transform.position = point;
-            }
-            
+            //timer -= Time.deltaTime;
+            //noteTimer = Convert.ToInt32(timer.ToString("0.0").Split('.')[0]);   
+            //if (noteTimer == 0)
+            //{
+            //    notelevel.GetComponent<Image>().sprite = Instantiate(NoteLevels[4], point, NotePrefab.transform.rotation) as Sprite;
+            //    notelevel.transform.position = point;
+            //}
         }
 
-            //按下鼠标左键时响应该方法 
-        if (isJump == false)
+        if (timer >= 0.1f)
+        {
+            timer -= Time.deltaTime;
+
+            for (int i = 0; i < 2; i++)
+            {
+                nums[0] = Convert.ToInt32(timer.ToString("0.0").Split('.')[0]) / 10;
+                nums[1] = Convert.ToInt32(timer.ToString("0.0").Split('.')[0]) % 10;
+                TimeNum.transform.GetChild(i).GetComponent<Image>().sprite = numbers[nums[i]];
+            }
+        }
+
+        HandleInput();
+        Miss();
+        ResetValues();
+
+    }
+
+    private void Miss()
+    {
+        if (isMiss)
+        {
+            Playeranim.SetTrigger("Miss");
+        }
+    }
+    private void ResetValues()
+    {
+        isMiss = false;
+    }
+
+    private void HandleInput()
+    {
+        if (!isJump)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Playeranim.SetInteger("State", 1);
+                isMiss = true;
             }
+        } 
 
-            if (Input.GetMouseButtonUp(0))
-            {
-                Playeranim.SetInteger("State", 0);
-            }
-        }
     }
 }
