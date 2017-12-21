5d8b94649a11fdd8edc46e68c72a398477d2515f
duanxiaoyun <cloudgaga750@gmail.com>
2017-12-21 22:24:26 +0900

    20171221
    20171221研究室 pushball

---
+++ OneTouch/Assets/_OneTouch/GameSystem/Scripts/UI/UIBaseView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBaseView : MonoBehaviour {
+
+    public int id;
+    private RectTransform m_RectTransform;
+    public RectTransform rectTransform{ get { return m_RectTransform ?? (m_RectTransform = GetComponent<RectTransform>()); } }
+
+    public bool activeSelf{ get { return gameObject.activeSelf; }}
+
+    public bool activeInHierarchy{ get { return gameObject.activeInHierarchy; } }
+
+    public void SetActive(bool value)
+    {
+        if(activeSelf!=value)
+            gameObject.SetActive(value);
+    }
+}
